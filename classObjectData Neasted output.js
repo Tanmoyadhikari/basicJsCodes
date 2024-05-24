76a6809519893ca4b364c78cad23e6c93c9af85a
Developer Tanmoy <85332765+Tanmoyadhikari@users.noreply.github.com>
2024-05-24 22:21:59 +0600

    Create classObjectData Neasted output.js

---
+++ classObjectData Neasted output.js
@@ -0,0 +1,42 @@
+<script>
+class Car {
+  constructor(name, year) {
+    this.name = name;
+    this.year = year;
+  }
+}
+
+ function myCar(){
+const person = {
+"person01":{
+  carName: "Volvo",
+  //lastName: "Doe",
+  age: 5,
+  eyeColor: "blue"
+},"person02":{
+  carName: "BMW",
+  //lastName: "Doe",
+  age: 3,
+  eyeColor: "blue"
+},"person03":{
+  carName: "KTM",
+  //lastName: "Doe",
+  age: 4,
+  eyeColor: "blue"
+}};
+for(let persons in person){
+let name = person[persons]["carName"];
+let year = 2024-person[persons]["age"];
+let myCar1 = new Car(name,year);
+document.write("Car Name :"+myCar1.name+"  ||  released :"+myCar1.year+"<br>")
+
+}
+ //document.getElementById("demo").innerHTML =
+//myCar1.name + " " + myCar1.year;
+document.write("<br><br>");
+return 0;
+};
+
+document.write(myCar());
+
+</script>
